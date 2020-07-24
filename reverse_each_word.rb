514cb2afc827eff2d9197d2619a0b3881aceae75
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-24 02:14:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
+require 'pry'
 def reverse_each_word(string)
   string_as_array = %w[string]
   turn_me_back = string_as_array.map { |i| i.reverse }
-  puts turn_me_back.to_s
+  p turn_me_back.to_s
+  binding.pry
 end
