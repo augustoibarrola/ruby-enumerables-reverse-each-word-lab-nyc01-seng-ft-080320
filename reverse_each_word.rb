68d2ebae90a36e4872ff8538549991772b14653c
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-24 02:24:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 require 'pry'
-def reverse_each_word("Hello there, and how are you?")
+variable = "Hello there, and how are you?"
+def reverse_each_word(variable)
   string_as_array = %w[string]
   turn_me_back = string_as_array.map { |i| i.reverse }
   p 'turn_me_back'.to_s
