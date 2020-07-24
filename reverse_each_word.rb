306ead26c30df5e71338ad6cba4facab50a9c639
Augusto Ibarrola <augusto.t.ibarrola@gmail.com>
2020-07-24 02:23:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 require 'pry'
-def reverse_each_word(string)
+def reverse_each_word("Hello there, and how are you?")
   string_as_array = %w[string]
   turn_me_back = string_as_array.map { |i| i.reverse }
   p 'turn_me_back'.to_s
